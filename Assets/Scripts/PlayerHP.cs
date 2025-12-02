6506d75006828eaa5d3334f2e95a0c2196930436
jinu217 <163497706+jinu217@users.noreply.github.com>
2025-12-02 14:59:14 +0900

    feat: playerHP UI

--- Assets/Scripts/PlayerHP.cs
+++ Assets/Scripts/PlayerHP.cs
@@ -1,18 +1,50 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerHP : MonoBehaviour
 {
     public int playerHP;
+    public int maxHP;
+
+    [Header("플레이어 HP UI")]
+    public Slider hpSlider;
+    public TextMeshProUGUI hpText;
 
     void Start()
     {
         playerHP = GameManager.Instance.playerHP;
-        Debug.Log("플레이어 시작 HP: " + playerHP);
+        maxHP = playerHP;
+
+        // HP 슬라이더 초기 세팅
+        if (hpSlider != null)
+        {
+            hpSlider.minValue = 0;
+            hpSlider.maxValue = maxHP;
+            hpSlider.value = playerHP;
+        }
+
+        UpdateUI();
     }
 
     void Update()
     {
         playerHP = GameManager.Instance.playerHP;
+
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        if (hpSlider != null)
+        {
+            hpSlider.value = playerHP;
+        }
+
+        if (hpText != null)
+        {
+            hpText.text = playerHP + "/" + maxHP;
+        }
     }
 
 }
